eabfb1a656e38624ad13dfa108b490937869042f
gotamyers <gotardo.fernando@gmail.com>
2019-09-26 16:01:10 +1000

    Organizing

--- Sensitivity/Read_scope_and_fft.py
+++ Sensitivity/Read_scope_and_fft.py
@@ -16,33 +16,36 @@ R = 50  # Resistance (ohms)
 L = 2 * mu0 * radius * Ncoils * (math.log10(16 * radius / dwire) - 2)  # Inductance
 nu_ref = 550 * 1e3  # Func. gen. driving freq.
 V_drive = 10/(2*math.sqrt(2)) # Voltage applied to the coil in root mean square
-RBW = 490  # Resolution bandwidth
+tal = 4
+RBW = 2*math.sqrt(6)/tal  # Resolution bandwidth
 I_driven = V_drive / math.sqrt(math.pow(R, 2) + math.pow((nu_ref * 2 * math.pi), 2) * math.pow(L, 2))
 B_ref = math.pow(4.5, 1.5) * mu0 * Ncoils * I_driven / radius
-
+freq_order = 100 #mininum frequency on a particular range
 ########################################################################################################################
 '''Read Oscilloscope'''
-with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\16thSep'
-          + '\\01_20.csv') as a:
-    df = csv.reader(a, delimiter=',')
-    df_temp = []
-    for row in df:
-        df_temp.append(row)
-    df = df_temp[21:-1]
-    for j in range(len(df)):
-        df[j] = [np.float(df[j][0]), np.float(df[j][1])]
-data['550_coupled'] = np.asarray(df)
+for i in range(10):
+    with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\20thSep'
+              + '\\100_to_1000_Hz\\2_' + str((i + 1)*freq_order) + '.csv') as a:
+        df = csv.reader(a, delimiter=',')
+        df_temp = []
+        for row in df:
+            df_temp.append(row)
+        df = df_temp[21:-1]
+        for j in range(len(df)):
+            df[j] = [np.float(df[j][0]), np.float(df[j][1])]
+    data['flux_far' + str(i + 1)] = np.asarray(df)
 
-with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\16thSep'
-          + '\\noise_5.csv') as a:
-    df = csv.reader(a, delimiter=',')
-    df_temp = []
-    for row in df:
-        df_temp.append(row)
-    df = df_temp[21:-1]
-    for j in range(len(df)):
-        df[j] = [np.float(df[j][0]), np.float(df[j][1])]
-data['550_noise'] = np.asarray(df)
+for i in range(5):
+    with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\20thSep'
+              + '\\100_to_1000_Hz\\n' + str(i) + '.csv') as a:
+        df = csv.reader(a, delimiter=',')
+        df_temp = []
+        for row in df:
+            df_temp.append(row)
+        df = df_temp[21:-1]
+        for j in range(len(df)):
+            df[j] = [np.float(df[j][0]), np.float(df[j][1])]
+    data['noise' + str(i)] = np.asarray(df)
 
 '''Start fourier transform (https://www.youtube.com/watch?v=su9YSmwZmPg)'''
 # n = len(df[:, 0])
@@ -59,47 +62,63 @@ data['550_noise'] = np.asarray(df)
 # plt.plot(freqs[mask], fft_theoretical[mask])
 
 '''Another fourier transform (https://stackoverflow.com/questions/25735153/plotting-a-fast-fourier-transform-in-python)'''
-N = len(data['550_noise'][:, 0])
-delta_t = data['550_noise'][1, 0] - data['550_noise'][0, 0]
+
+N = len(data['noise4'][:, 0])
+delta_t = data['noise4'][1, 0] - data['noise4'][0, 0]
 
 data['freq'] = np.fft.fftfreq(N, delta_t)
 mask = data['freq'] > 0
 
-data['FFT_550_noise'] = np.fft.fft(data['550_noise'][:, 1])
-data['FFT_550_noise_theoretical'] = 2.0*np.abs(data['FFT_550_noise'] / N)
+data['FFT_noise4'] = np.fft.fft(data['noise4'][:, 1])
+data['FFT_noise4_theoretical'] = 2.0*np.abs(data['FFT_noise4'] / N)
 
-data['FFT_550_coupled'] = np.fft.fft(data['550_coupled'][:, 1])
-data['FFT_550_coupled_theoretical'] = 2.0*np.abs(data['FFT_550_coupled'] / N)
+for i in range(10):
+    data['FFT_flux_far' + str(i + 1)] = np.fft.fft(data['flux_far' + str(i + 1)][:, 1])
+    data['FFT_flux_far' + str(i + 1) + '_theoretical'] = 2.0*np.abs(data['FFT_flux_far' + str(i + 1)] / N)
 
-driven_freq = np.where((data['freq'] < 25000) & (data['freq'] > 15000))
-print(driven_freq)
+    driven_freq = np.where((data['freq'] > ((i+1) - 0.1)*freq_order) & (data['freq'] < ((i+1) + 0.1)*freq_order))
+    # print(driven_freq)
 
-coupled_max = data['FFT_550_coupled_theoretical'][31:50].max()
-noise_max = data['FFT_550_noise_theoretical'][31:50].max()
+    signal = data['FFT_flux_far' + str(i + 1) + '_theoretical'] - data['FFT_noise4_theoretical']
+    flux_max = signal[driven_freq].max()
+    noise_max = data['FFT_noise4_theoretical'][driven_freq].max()
 
-SNR = coupled_max/noise_max
+    data['SNR' + str(i+1)] = flux_max/noise_max
 
-B = B_ref/(np.sqrt((SNR*RBW)))
+    data['Bmin' + str(i+1)] = B_ref/(np.sqrt((data['SNR' + str(i+1)]*RBW)))
 # print('Number os indexes is : ', len(data['FFT_550_coupled_theoretical']))
-print('Sensitivity is: ', B)
+    print('Sensitivity is: ', data['Bmin' + str(i+1)])
 # print('Noise is: ', noise_max)
 # print('Signal is: ', coupled_max)
 
 ########################################################################################################################
 '''SAVE DICTIONARY'''
-# pickle_out = open("scope_2mm_160919.pickle", "wb")
+# pickle_out = open("2mmflux_far_1to10Hz.pickle", "wb")
 # pickle.dump(data, pickle_out)
 # pickle_out.close()
 ########################################################################################################################
 
-
-plt.figure()
-plt.plot(1e-3*data['freq'][mask], 1e3*data['FFT_550_coupled_theoretical'][mask])
-plt.xlim(10, 100)
-plt.ylim(0, 0.40)
-
-plt.xlabel('Frequency (kHz)')
-plt.ylabel('SNR')
-plt.title('20 khz - far')
-
-plt.show()
+# for i in range(10):
+#     plt.figure(i+1)
+#     plt.plot(data['freq'][mask], 1e3*data['FFT_flux_far' + str(i + 1) + '_theoretical'][mask])
+#     plt.xscale('log')
+#     plt.yscale('log')
+#     plt.xlim(1, 1000)
+#     plt.ylim(0., 0.80)
+#
+#     plt.xlabel('Frequency (Hz)')
+#     plt.ylabel('mV/Hz')
+#     # plt.title('20 khz - far')
+#
+# plt.figure(12)
+# plt.plot(data['freq'][mask], 1e3*data['FFT_noise4_theoretical'][mask])
+# plt.xlim(0.5, 10.5)
+# plt.ylim(0, 0.80)
+# # plt.yscale('log')
+# # plt.xscale('log')
+#
+# plt.xlabel('Frequency (Hz)')
+# plt.ylabel('mV')
+# plt.title('Noise')
+#
+# plt.show()
--- Sensitivity/test.py
+++ Sensitivity/test.py
@@ -1,115 +1,26 @@
-import csv
-import math
 import numpy as np
 import pickle
 
 data = {}
 
-'''Calculating helmholtz coil magnetic field'''
-mu0 = 4 * math.pi * 1e-7  # Magnetic permeability vacuum
-Ncoils = 10  # Number of turns
-dwire = 0.8  # Wires thickness
-radius = 0.03  # Coil radius
-R = 50  # Resistance (ohms)
-L = 2 * mu0 * radius * Ncoils * (math.log10(16 * radius / dwire) - 2)  # Inductance
-nu_ref = 550 * 1e3  # Func. gen. driving freq.
-V_drive = 27
-RBW = 30  # Resolution bandwidth
-I_driven = V_drive / math.sqrt(math.pow(R, 2) + math.pow((nu_ref * 2 * math.pi), 2) * math.pow(L, 2))
-B_ref = math.pow(4.5, 1.5) * mu0 * Ncoils * I_driven / radius
+pickle_in = open("2mmflux_close_1000to10000Hz.pickle", "rb")
+data_close_1000to10000Hz = pickle.load(pickle_in)
 
+pickle_in = open("2mmflux_far_1000to10000Hz.pickle", "rb")
+data_far_1000to10000Hz = pickle.load(pickle_in)
 
-########################################################################################################################
-'''Read Spectrum analyzer, find SNR and calculate S_NN in not dB'''
+sensitivity_close_1000to10000Hz = []
+sensitivity_far_1000to10000Hz = []
 
-for k in range(2):
-    # with open('C:\\Users\\Fernando\\Documents\\Phd'
-    #           + '\\sensitivity_060919_FG\\SSA_' + str('{:02d}'.format(k+1)) + '_540.csv') as a:
-    with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\11thSep'
-              + '\\SSA_' + str('{:02d}'.format(k+1)) + '_550.csv') as a:
-        df = csv.reader(a, delimiter=',')
-        df_temp = []
-        for row in df:
-            df_temp.append(row)
-        df = df_temp[30:]
+for i in range(10):
+    sensitivity_close_1000to10000Hz = np.append(sensitivity_close_1000to10000Hz,
+                                                data_close_1000to10000Hz['Bmin' + str(i + 1)])
+    sensitivity_far_1000to10000Hz = np.append(sensitivity_far_1000to10000Hz,
+                                              data_far_1000to10000Hz['Bmin' + str(i + 1)])
 
-        for j in range(len(df)):
-            df[j] = [np.float(df[j][0]), np.float(df[j][1])]
+data['sensitivity_close_1000to10000Hz'] = sensitivity_close_1000to10000Hz
+data['sensitivity_far_1000to10000Hz'] = sensitivity_far_1000to10000Hz
 
-        data['SSA_' + str('{:02d}'.format(k+1)) + '_550'] = np.reshape(np.array(df), (-1, 2))
-
-    data['SSA_' + str('{:02d}'.format(k+1)) + '_550'] = np.array(df)
-    data['SSA_' + str('{:02d}'.format(k+1)) + 'ref'] = data['SSA_' + str('{:02d}'.format(k+1))
-                                                            + '_550'][360:380, 1].max()
-
-# with open('C:\\Users\\Fernando\\Documents\\Phd'
-#           + '\\sensitivity_060919_FG\\SSA_01_ref.csv') as a:
-with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\11thSep'
-          + '\\SSA_01.csv') as a:
-    df = csv.reader(a, delimiter=',')
-    df_temp = []
-    for row in df:
-        df_temp.append(row)
-    df = df_temp[30:]
-
-    for j in range(len(df)):
-        df[j] = [np.float(df[j][0]), np.float(df[j][1])]
-
-    data['SSA_01_noise'] = np.reshape(np.array(df), (-1, 2))
-
-data['SSA_01_noise'] = np.array(df)
-data['SSA_noise'] = data['SSA_01_noise'][360:380, 1].max()
-data['S_NN_noise'] = np.power(10, np.divide(data['SSA_01_noise'][:, 1], 10))
-data['S_NN_noise'] = np.divide(data['S_NN_noise'], data['S_NN_noise'][360:380].max())
-
-'''SNR'''
-for k in range(2):
-    data['SNR_' + str('{:02d}'.format(k+1))] = np.power(10, np.divide(data['SSA_' + str('{:02d}'.format(k+1)) +
-                                                                           'ref'] - data['SSA_noise'], 10))
-
-########################################################################################################################
-'''Read Network analyzer and calculate S_21 in not dB'''
-for k in range(2):
-    # with open('C:\\Users\\Fernando\\Documents\\Phd'
-    #           + '\\sensitivity_060919_FG\\TRACE' + str('{:02d}'.format(k+1)) + '.csv') as a:
-    with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\11thSep'
-              + '\\TRACE' + str('{:02d}'.format(k+1)) + '.csv') as a:
-
-        df = csv.reader(a, delimiter=',')
-        df_temp = []
-
-        for row in df:
-            df_temp.append(row)
-        df = df_temp[3:]
-
-        for j in range(len(df)):
-            df[j] = [np.float(df[j][0]), np.float(df[j][1])]
-
-        data['TRACE' + str(k + 1)] = np.reshape(np.array(df), (-1, 2))
-    data['TRACE' + str(k + 1)] = np.array(df)
-
-    data['S21_' + str(k + 1)] = np.power(10, np.divide(data['TRACE' + str(k + 1)][:, 1], 10))
-    data['S21_' + str(k + 1)] = np.divide(data['S21_' + str(k + 1)], data['S21_' + str(k + 1)][367])
-    data['SSA_' + str('{:02d}'.format(k + 1)) + 'ref'] = np.power(10, np.divide(data['SSA_' + str('{:02d}'.format(k+1))
-                                                                                     + 'ref'], 10))
-    data['S_NN_' + str(k + 1)] = np.power(10, np.divide(data['SSA_' + str('{:02d}'.format(k+1)) + '_550'][:, 1], 10))
-    data['S_NN_' + str(k + 1)] = np.divide(data['S_NN_' + str(k + 1)], data['SSA_' + str('{:02d}'.format(k+1)) + 'ref'])
-
-
-########################################################################################################################
-'''Calculate the Sensitivity in function of frequency'''
-ymax = np.zeros(2)
-
-for k in range(2):
-    data['Bmin' + str(k + 1)] = np.sqrt(np.divide(data['S_NN_noise'], data['S21_' + str(k + 1)])) * B_ref
-    data['Bmin' + str(k + 1)] = np.divide(data['Bmin' + str(k + 1)],
-                                          np.sqrt(np.multiply(data['SNR_' + str('{:02d}'.format(k+1))], RBW)))
-    data['Bmin_min' + str(k + 1)] = data['Bmin' + str(k + 1)].min()
-
-
-########################################################################################################################
-'''SAVE DICTIONARY'''
-pickle_out = open("sensitivity_old.pickle", "wb")
+pickle_out = open("sensitivity_1000to10000Hz.pickle", "wb")
 pickle.dump(data, pickle_out)
 pickle_out.close()
-########################################################################################################################
