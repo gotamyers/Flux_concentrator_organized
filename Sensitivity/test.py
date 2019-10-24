27dd4fc10b31ed9de5802dcb7856d44d5e4f8dab
gotamyers <gotardo.fernando@gmail.com>
2019-10-24 16:33:12 +1000

    Organizing

--- Sensitivity/test.py
+++ Sensitivity/test.py
@@ -1,8 +1,7 @@
 from scipy.misc import derivative
 
 def f(x):
-    # o = (9.21113854e4 - 5.10351812e2)/(1. - 1j*x*np.power(x, -2.39505646e-1)*pow(4.63152449e-6, 1-2.39505646e-1)*np.exp(1j*2.39505646e-1*np.pi/2)) + 5.10351812e2
-    o = np.cos(x)
+    o = (9.21113854e4 - 5.10351812e2)/(1. - 1j*x*np.power(x, -2.39505646e-1)*pow(4.63152449e-6, 1-2.39505646e-1)*np.exp(1j*2.39505646e-1*np.pi/2)) + 5.10351812e2
     return np.real(o)
 
 def d(x):
@@ -14,10 +13,10 @@ def d2(x):
 import matplotlib.pyplot as plt
 import numpy as np
 
-y = np.linspace(0, 10, 10000)
+y = np.linspace(1e3, 1e8, 1000000)
 plt.figure(1)
 plt.plot(y, f(y))
-# plt.xscale('log')
+plt.xscale('log')
 
 plt.figure(2)
 plt.plot(y,d(y))
