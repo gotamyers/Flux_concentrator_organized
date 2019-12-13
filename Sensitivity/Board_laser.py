c1d77232c2a0c289e91a7b7bd5a5748bbb328445
gotamyers <gotardo.fernando@gmail.com>
2019-12-13 15:54:59 +1000

    Organizing

--- Sensitivity/Board_laser.py
+++ Sensitivity/Board_laser.py
@@ -43,10 +43,24 @@ for k in range(7):
     data['LPD_V' + str(k + 1)] = np.power(10, data['LPD' + str(k + 1)][:, 1]/10)
 data['LPD1'] = data['retakenLPD1']
 data['LPD2'] = data['retakenLPD2']
+data['LPD_V1'] = np.power(10, data['retakenLPD1'][:, 1]/10)
+data['LPD_V2'] = np.power(10, data['retakenLPD2'][:, 1]/10)
 
+with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Sensitivity_calculations\\254_4\\29thNov'
+          + '\\SSA_00.csv') as a:
+    df = csv.reader(a, delimiter=',')
+    df_temp = []
+    for row in df:
+        df_temp.append(row)
+    df = df_temp[31:]
+    for j in range(len(df)):
+        df[j] = [np.float(df[j][0]), np.float(df[j][1])]
+
+data['SA_noise'] = np.array(df)
+data['SA_noise_V'] = np.power(10, data['SA_noise'][:, 1] / 10)
 ########################################################################################################################
 '''SAVE DICTIONARY'''
-# pickle_out = open("2mmflux_SA_funcgen_10to1000khz.pickle", "wb")
+# pickle_out = open("board_laser_attenuator_charact.pickle", "wb")
 # pickle.dump(data, pickle_out)
 # pickle_out.close()
 ########################################################################################################################
@@ -64,13 +78,18 @@ plt.xlabel('Frequency (Hz)')
 plt.ylabel('PSD (dB)')
 plt.title('Spectrum analyser signal')
 
-# plt.figure(2)
-# plt.plot(data['LPD2'][:, 0], data['LPD2'][:, 1], label='LPD1', color='violet')
-# plt.plot(data['retakenLPD2'][:, 0], data['retakenLPD2'][:, 1], label='retaken1', color='black')
-# plt.legend(loc='upper right')
-# plt.xlabel('Frequency (Hz)')
-# plt.ylabel('PSD (dB)')
-# plt.title('Spectrum analyser signal')
+plt.figure(2)
+
+plt.plot(data['LPD7'][:, 0], data['LPD7'][:, 1], label='0.0 mW')
+plt.plot(data['SA_noise'][:, 0], data['SA_noise'][:, 1], label='SA noise')
+plt.xlim(9000, 100e6)
+plt.ylim(-125, -65)
+# plt.xscale('log')
+# plt.yscale('log')
+plt.legend(loc='upper right')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('PSD (dB)')
+plt.title('Spectrum analyser signal')
 
 plt.show()
 
