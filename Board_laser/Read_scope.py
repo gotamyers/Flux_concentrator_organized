844370f9a6819f0aa834efa66f946d07368db701
gotamyers <gotardo.fernando@gmail.com>
2020-02-25 08:53:03 +1000

    Organizing

--- Board_laser/Read_scope.py
+++ Board_laser/Read_scope.py
@@ -5,12 +5,65 @@ import pickle
 from numpy.fft import fft, fftfreq
 import matplotlib.pyplot as plt
 
+# This script is supposed to read .csv files saved with the oscilloscope, take the average of 10 measurements made for
+# each attenuated power, subtract the electronical noise and plot PSD in function of frequency.
+
 data = {}
 
+'''Read data'''
 for i in range(7):
-    with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\18thFeb2020'
-              + '\\sc_0' + str(i) + '_1.csv') as a:
+    for k in range(10): #10 measurements with same configuration (k = 1 to 10)
+        with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\18thFeb2020'
+                  + '\\sc_0' + str(i) + '_' + str(k + 1) + '.csv') as a:
 
+            df = csv.reader(a, delimiter=',')
+            df_temp = []
+            for row in df:
+                df_temp.append(row)
+            df = df_temp[3:]
+            for j in range(len(df)):
+                df[j] = [np.float(df[j][0]), np.float(df[j][1])]
+
+        data['scope_' + str(i) + '_' + str(k + 1)] = np.asarray(df)
+
+
+N = len(data['scope_0_1'][:, 0])
+delta_t = data['scope_0_1'][1, 0] - data['scope_0_1'][0, 0]
+
+data['freq'] = np.fft.fftfreq(N, delta_t)
+mask = data['freq'] > 0
+
+'''F.T of the amplitude'''
+for i in range(7):
+    for k in range(10):
+        data['FFT_scope_' + str(i) + str(k + 1)] = np.fft.fft(data['scope_' + str(i) + '_' + str(k + 1)][:, 1])
+        data['FFT_scope_' + str(i) + str(k + 1) + '_theo'] = 2.0*(np.abs(data['FFT_scope_' + str(i) + str(k + 1)]/N))**2
+        data['dB_FFT_scope_' + str(i) + str(k + 1) + '_theo'] = 10*np.log10(data['FFT_scope_' + str(i) + str(k + 1) + '_theo'])
+
+'''Averaging PSD'''
+for i in range(7):
+    sum = np.zeros(len(data['dB_FFT_scope_01_theo']))
+    for k in range(10):
+        sum = sum + data['dB_FFT_scope_' + str(i) + str(k + 1) + '_theo']
+    data['avg_dB_FFT_scope_' + str(i) + '_theo'] = sum/10
+
+
+
+plt.figure(1)
+for i in range(7):
+    # plt.figure(i)
+    plt.plot(data['freq'][mask], data['avg_dB_FFT_scope_' + str(i) + '_theo'][mask])
+    # plt.xlim(50000, 1050000)
+    # plt.ylim(-160, -60)
+
+    plt.xlabel('Frequency (Hz)')
+    # plt.ylabel('SNR')
+    # plt.title('20 khz - far')
+
+# Here, I am doing similar procedure but only for scopes noise, also averaging
+for k in range(10):
+    with open('C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\18thFeb2020'
+              + '\\sc_noise_' + str(k + 1) + '.csv') as a:
         df = csv.reader(a, delimiter=',')
         df_temp = []
         for row in df:
@@ -19,36 +72,23 @@ for i in range(7):
         for j in range(len(df)):
             df[j] = [np.float(df[j][0]), np.float(df[j][1])]
 
-    data['scope_0' + str(i)] = np.asarray(df)
+    data['scope_noise' + str(k + 1)] = np.asarray(df)
 
-N = len(data['scope_00'][:, 0])
-delta_t = data['scope_00'][1, 0] - data['scope_00'][0, 0]
+for k in range(10):
+    data['FFT_scope_noise' + str(k + 1)] = np.fft.fft(data['scope_noise' + str(k + 1)][:, 1])
+    data['FFT_scope_noise' + str(k + 1) + '_theo'] = 2.0*(np.abs(data['FFT_scope_noise' + str(k + 1)] / N))**2
+    data['dB_FFT_scope_noise' + str(k + 1) + '_theo'] = 10*np.log10(data['FFT_scope_noise' + str(k + 1) + '_theo'])
 
-data['freq'] = np.fft.fftfreq(N, delta_t)
-mask = data['freq'] > 0
+'''Averaging scope noise'''
+sum = np.zeros(len(data['dB_FFT_scope_noise1_theo']))
+for k in range(10):
+    sum = sum + data['dB_FFT_scope_noise' + str(k + 1) + '_theo']
+data['avg_dB_FFT_scope_noise_theo'] = sum / 10
 
-data['FFT_scope_00'] = np.fft.fft(data['scope_00'][:, 1])
-# data['FFT_noise4_theo'] = 20.*np.log10(np.abs(data['FFT_noise4'] / N))
-data['FFT_scope_00_theo'] = (2.0*np.abs(data['FFT_scope_00'] / N))**2
-
-
-for i in range(7):
-    data['FFT_scope_0' + str(i)] = np.fft.fft(data['scope_0' + str(i)][:, 1])
-    # data['FFT_flux_far_' + str((i+1)) + '_theo'] = 20.*np.log10(np.abs(data['FFT_flux_far_' + str((i+1))] / N))
-    data['FFT_scope_0' + str(i) + '_theo'] = 2.0*(np.abs(data['FFT_scope_0' + str(i)] / N))**2
-    data['dB_FFT_scope_0' + str(i) + '_theo'] = 10*np.log10(data['FFT_scope_0' + str(i) + '_theo'])
-
-
-
-for i in range(7):
-    plt.figure(i)
-    plt.plot(data['freq'][mask], data['dB_FFT_scope_0' + str(i) + '_theo'][mask])
-    # plt.xlim(50000, 1050000)
-    # plt.ylim(-160, -60)
-
-    plt.xlabel('Frequency (Hz)')
-    # plt.ylabel('SNR')
-    # plt.title('20 khz - far')
+'''Plot test'''
+plt.figure(10)
+plt.plot(data['freq'][mask], data['avg_dB_FFT_scope_noise_theo'][mask])
+plt.plot(data['freq'][mask], data['avg_dB_FFT_scope_0_theo'][mask])
 
 plt.show()
 
