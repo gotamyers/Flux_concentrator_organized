709f579aef139df9de6b96300f0128a795577bc6
gotamyers <gotardo.fernando@gmail.com>
2020-02-27 18:35:18 +1000

    Organizing

--- Board_laser/Plot_scope_bigdata.py
+++ Board_laser/Plot_scope_bigdata.py
@@ -8,6 +8,9 @@ def smooth(y, box_pts):
     y_smooth = np.convolve(y, box, mode='same')
     return y_smooth
 
+
+# smoothed_data = {}
+
 power = [0.0, 0.25, 0.50, 0.75, 1.00, 1.25, 1.50]
 n_smooth = 300
 freq_point = 50e3
@@ -17,6 +20,7 @@ data0 = pickle.load(pickle_in)
 
 freq = np.where(np.isclose(data0['freq'], freq_point))
 LPD_V_notdB = np.zeros(len(power))
+# smoothed_data['freq'] = data0['freq']
 
 mask = data0['freq'] > 0
 elec_noise = smooth(np.power(10, data0['dB_avg_FFT_scope_theo']/10), n_smooth)
@@ -24,14 +28,19 @@ elec_noise = smooth(np.power(10, data0['dB_avg_FFT_scope_theo']/10), n_smooth)
 for i in range(7):
     pickle_in = open("dB_avg_FFT_scope_theo_" + str(i + 1) + ".pickle", "rb")
     data_plot = pickle.load(pickle_in)
-    # mask = data_plot['freq'] > 0
-    # a1 = data_plot['freq'][mask]
-    # print(a1[3] - a1[2])
 
     '''Smoothing curve so that I can subtract electronical noise'''
     data_plot['smooth_dB_avg_FFT_scope_theo'] = smooth(np.power(10, data_plot['dB_avg_FFT_scope_theo']/10), n_smooth)
     data_plot['smooth_avg_FFT_scope_theo'] = np.abs(data_plot['smooth_dB_avg_FFT_scope_theo'] - elec_noise)
     data_plot['smooth_dB_avg_FFT_scope_theo'] = 10*np.log10(data_plot['smooth_avg_FFT_scope_theo'])
+    # smoothed_data['smooth_avg_FFT_scope_theo'] = data_plot['smooth_avg_FFT_scope_theo']
+
+    # ################################################################################################################
+    # '''SAVE DICTIONARY'''
+    # pickle_out = open("smoothed_data" + str(i + 1) + ".pickle", "wb")
+    # pickle.dump(smoothed_data, pickle_out)
+    # pickle_out.close()
+    # ################################################################################################################
 
     LPD_V_notdB[i] = data_plot['smooth_avg_FFT_scope_theo'][freq]
 
--- Board_laser/Test_board_laser.py
+++ Board_laser/Test_board_laser.py
@@ -1,22 +1,50 @@
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.polynomial.polynomial as poly
 
-pickle_in = open("avg_dB_FFT_noise.pickle", "rb")
-data_noise = pickle.load(pickle_in)
+pickle_in = open("smoothed_data1.pickle", "rb")
+data1 = pickle.load(pickle_in)
 
-pickle_in = open("avg_dB_FFT0_plot.pickle", "rb")
-data = pickle.load(pickle_in)
+power = [0.0, 0.25, 0.50, 0.75, 1.00, 1.25, 1.50]
 
-mask = data_noise['freq'] > 0
+f0 = np.where(np.isclose(data1['freq'], 10000, atol=10))
+ff = np.where(np.isclose(data1['freq'], 1.00001e6, atol=10))
+freq_index = (np.where((data1['freq'] >= f0) & (data1['freq'] <= ff)))
+freq = data1['freq'][f0:ff]
+
+coef_x_square = np.zeros(len(freq))
+coef_x_lin = np.zeros(len(freq))
+
+for k in freq_index:
+    LPD_V_notdB = np.zeros(len(power))
+    for i in range(len(power)):
+        pickle_in = open("smoothed_data" + str(i+1) + ".pickle", "rb")
+        data = pickle.load(pickle_in)
+
+        LPD_V_notdB[i] = data['smooth_avg_FFT_scope_theo'][freq_index]
+
+    '''Fitting polynomial to figure (2)'''
+    x_new = np.linspace(power[0], power[-1], num=len(power) * 10)
+    coefs = poly.polyfit(power, LPD_V_notdB, 2)
+    ffit = poly.Polynomial(coefs)
+    # plt.plot(x_new, ffit(x_new))
+    coef_x_square[k] = coefs[0]
+    coef_x_lin[k] = coefs[1]
 
 
-plt.figure(1)
-plt.plot(data_noise['freq'][mask], data['avg_dB_FFT_scope_noise_theo'][mask])
-plt.plot(data_noise['freq'][mask], data_noise['avg_dB_FFT_scope_theo'][mask])
 
-plt.xlim(9e3, 0.1e6)
-plt.ylim(-80, -50)
 
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
 
