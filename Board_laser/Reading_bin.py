d6d2847cc65f4282a9fccca66f2b94133e9f8e89
Fernando <gotardo.fernando@gmail.com>
2020-03-23 14:19:58 +1000

    ...

--- Board_laser/Reading_bin.py
+++ Board_laser/Reading_bin.py
@@ -7,7 +7,7 @@ def smooth(y, box_pts):
     return y_smooth
 
 
-n_smooth = 300
+n_smooth = 50
 
 data = np.fromfile("C:\\Users\\Fernando\\Documents\\Phd\\Board_laser\\21thMarch2020\\testPSD001.bin", dtype='float', count=-1)
 PSD1 = data[2:]
@@ -19,10 +19,10 @@ f0 = data[0]
 df = data[1]
 freq = f0 + df*np.array(range(len(PSD1)))
 
-PSD1_not_dB = smooth(np.power(10, PSD1/10), n_smooth)
-# PSD2_not_dB = np.power(10, PSD2/10)
-# PSD3_not_dB = np.power(10, PSD3/10)
-# PSD4_not_dB = np.power(10, PSD4/10)
+# PSD1_not_dB = smooth(np.power(10, PSD1/10), n_smooth)
+# PSD2_not_dB = smooth(np.power(10, PSD2/10), n_smooth)
+# PSD3_not_dB = smooth(np.power(10, PSD3/10), n_smooth)
+# PSD4_not_dB = smooth(np.power(10, PSD4/10), n_smooth)
 #
 # PSD1_not_dB = PSD1_not_dB - PSD4_not_dB
 # PSD2_not_dB = PSD2_not_dB - PSD4_not_dB
@@ -31,7 +31,7 @@ PSD1_not_dB = smooth(np.power(10, PSD1/10), n_smooth)
 # PSD1 = 10*np.log10(PSD1_not_dB)
 # PSD2 = 10*np.log10(PSD2_not_dB)
 # PSD3 = 10*np.log10(PSD3_not_dB)
-
+# PSD4 = 10*np.log10(PSD4_not_dB)
 
 plt.plot(freq, PSD1, color='blue')
 plt.plot(freq, PSD2, color='red')
