2b43ae3f54b7aa180aaaf6402bd2b006678ef070
gotamyers <gotardo.fernando@gmail.com>
2020-04-13 13:42:39 +1000

    Organizing

--- Board_laser/Reading_bin.py
+++ Board_laser/Reading_bin.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+import numpy.polynomial.polynomial as poly
 
 def smooth(y, box_pts):
     box = np.ones(box_pts)/box_pts
@@ -7,46 +9,137 @@ def smooth(y, box_pts):
     return y_smooth
 
 
-n_smooth = 10
+data = {}
+n_smooth = 100
+power = [192, 152.8, 120, 91.4, 63.4, 33.27]
 
-# data = np.fromfile("C:\\Users\\Fernando\\Documents\\Phd\\Board_laser\\21thMarch2020\\testPSD001.bin", dtype='float', count=-1)
-# PSD1 = data[2:]
-# PSD2 = np.fromfile("C:\\Users\\Fernando\\Documents\\Phd\\Board_laser\\21thMarch2020\\testPSD002.bin", dtype='float', count=-1)[2:]
-# PSD3 = np.fromfile("C:\\Users\\Fernando\\Documents\\Phd\\Board_laser\\21thMarch2020\\testPSD003.bin", dtype='float', count=-1)[2:]
-# PSD4 = np.fromfile("C:\\Users\\Fernando\\Documents\\Phd\\Board_laser\\21thMarch2020\\testPSD004.bin", dtype='float', count=-1)[2:]
+ref_data = np.fromfile("C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\9thApr2020\\testPSD020.bin", dtype='float', count=-1)
+data["freq"] = ref_data[0] + ref_data[1]*np.array(range(len(ref_data[2:])))
+data["shot_noise"] = np.fromfile("C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\9thApr2020\\testPSD020.bin", dtype='float', count=-1)[2:]
+data["elec_noise"] = np.fromfile("C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\9thApr2020\\testPSD021.bin", dtype='float', count=-1)[2:]
 
-data = np.fromfile("C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\21thMar2020\\testPSD001.bin", dtype='float', count=-1)
-PSD1 = data[2:]
-PSD2 = np.fromfile("C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\21thMar2020\\testPSD002.bin", dtype='float', count=-1)[2:]
-PSD3 = np.fromfile("C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\21thMar2020\\testPSD003.bin", dtype='float', count=-1)[2:]
-PSD4 = np.fromfile("C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\21thMar2020\\testPSD004.bin", dtype='float', count=-1)[2:]
+data["smooth_dB_shot_noise"] = 10*np.log10(smooth(np.power(10, data["shot_noise"]/10), n_smooth))
+data["smooth_dB_elec_noise"] = 10*np.log10(smooth(np.power(10, data["elec_noise"]/10), n_smooth))
 
-f0 = data[0]
-df = data[1]
-freq = f0 + df*np.array(range(len(PSD1)))
+for k in [6, 9, 11, 13, 15, 17]:
+    data["PSD_phase" + str(k)] = np.fromfile(
+        "C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\9thApr2020\\testPSD0{0}.bin".format(
+            "{:02d}".format(k)), dtype='float', count=-1)[2:]
 
-# PSD1_not_dB = smooth(np.power(10, PSD1/10), n_smooth)
-# PSD2_not_dB = smooth(np.power(10, PSD2/10), n_smooth)
-# PSD3_not_dB = smooth(np.power(10, PSD3/10), n_smooth)
-# PSD4_not_dB = smooth(np.power(10, PSD4/10), n_smooth)
+    data["smooth_PSD_phase" + str(k)] = smooth(np.power(10, data["PSD_phase" + str(k)]/10), n_smooth)
+    data["smooth_dB_PSD_phase" + str(k)] = 10*np.log10(data["smooth_PSD_phase" + str(k)])
+
+for k in [7, 10, 12, 14, 16, 18]:
+    data["PSD_amp" + str(k)] = np.fromfile(
+        "C:\\Users\\uqfgotar\\Documents\\Magnetometry\\Board_laser\\9thApr2020\\testPSD0{0}.bin".format(
+            "{:02d}".format(k)), dtype='float', count=-1)[2:]
+
+    data["smooth_PSD_amp" + str(k)] = smooth(np.power(10, data["PSD_amp" + str(k)]/10), n_smooth)
+    data["smooth_dB_PSD_amp" + str(k)] = 10*np.log10(data["smooth_PSD_amp" + str(k)])
+
+########################################################################################################################
+'''SAVE DICTIONARY'''
+# pickle_out = open("phase_amp_noise_board_laser.pickle", "wb")
+# pickle.dump(data, pickle_out)
+# pickle_out.close()
+########################################################################################################################
+'''fit coefficients for quantum and classical noise'''
+
+coef_x_square_phase = np.zeros(len(data["freq"]))
+coef_x_lin_phase = np.zeros(len(data["freq"]))
+
+for k in range(len(data['freq'])):
+    LPD_V_notdB = np.zeros(len(power))
+    a=0
+    for i in [6, 9, 11, 13, 15, 17]:
+        LPD_V_notdB[a] = data["smooth_PSD_phase" + str(i)][k]
+        a=a+1
+
+    '''Fitting polynomial to figure (2)'''
+    x_new = np.linspace(power[0], power[-1], num=len(power) * 10)
+    coefs = poly.polyfit(power, LPD_V_notdB, 2)
+    ffit = poly.Polynomial(coefs)
+    # plt.plot(x_new, ffit(x_new))
+    coef_x_square_phase[k] = coefs[2]
+    coef_x_lin_phase[k] = coefs[1]
+
+ratio_phase = coef_x_square_phase/coef_x_lin_phase
+
+coef_x_square_amp = np.zeros(len(data["freq"]))
+coef_x_lin_amp = np.zeros(len(data["freq"]))
+for k in range(len(data['freq'])):
+    LPD_V_notdB = np.zeros(len(power))
+    a=0
+    for i in [7, 10, 12, 14, 16, 18]:
+        LPD_V_notdB[a] = data["smooth_PSD_amp" + str(i)][k]
+        a=a+1
+
+    '''Fitting polynomial to figure (2)'''
+    x_new = np.linspace(power[0], power[-1], num=len(power) * 10)
+    coefs = poly.polyfit(power, LPD_V_notdB, 2)
+    ffit = poly.Polynomial(coefs)
+    # plt.plot(x_new, ffit(x_new))
+    coef_x_square_amp[k] = coefs[2]
+    coef_x_lin_amp[k] = coefs[1]
+
+ratio_amp = coef_x_square_amp/coef_x_lin_amp
+
+
+# a = 0
+# plt.figure(1)
+# for k in [6, 9, 11, 13, 15, 17]:
+#     plt.plot(data["freq"], data["smooth_dB_PSD_phase" + str(k)], label=str(power[a]))
+#     a = a+1
+# plt.plot(data["freq"], data["smooth_dB_shot_noise"], label='shot_noise')
+# plt.plot(data["freq"], data["smooth_dB_elec_noise"], label='elec_noise')
 #
-# PSD1_not_dB = PSD1_not_dB - PSD4_not_dB
-# PSD2_not_dB = PSD2_not_dB - PSD4_not_dB
-# PSD3_not_dB = PSD3_not_dB - PSD4_not_dB
+# plt.xlim(0, 2e5)
 #
-# PSD1 = 10*np.log10(PSD1_not_dB)
-# PSD2 = 10*np.log10(PSD2_not_dB)
-# PSD3 = 10*np.log10(PSD3_not_dB)
-# PSD4 = 10*np.log10(PSD4_not_dB)
+# plt.xlabel('Frequency (Hz)')
+# plt.ylabel('PSD (dB)')
+# plt.title("Phase noise")
+# plt.legend(loc='upper right')
+#
+# a=0
+# plt.figure(2)
+# for k in [7, 10, 12, 14, 16, 18]:
+#     plt.plot(data["freq"], data["smooth_dB_PSD_amp" + str(k)], label=str(power[a]))
+#     a = a+1
+# plt.plot(data["freq"], data["smooth_dB_shot_noise"], label='shot_noise')
+# plt.plot(data["freq"], data["smooth_dB_elec_noise"], label='elec_noise')
+#
+# plt.xlim(0, 2e5)
+#
+# plt.xlabel('Frequency (Hz)')
+# plt.ylabel('PSD (dB)')
+# plt.title("Amplitude noise")
+# plt.legend(loc='upper right')
+#
+plt.figure(3)
+plt.plot(data['freq'], coef_x_square_phase, label='square', color='b')
+plt.plot(data['freq'], coef_x_lin_phase, label='linear', color='red')
+# plt.plot(data['freq'], ratio_phase)
 
-plt.plot(1e-6*freq, PSD1, color='blue', label='phase noise')
-plt.plot(1e-6*freq, PSD2, color='red', label='amp noise')
-plt.plot(1e-6*freq, PSD3, color='k', label='shot noise')
-plt.plot(1e-6*freq, PSD4, color='olive', label='elect noise')
-plt.xlim(0, 1.2)
+plt.xlim(0, 2e5)
+plt.ylim(0, 1e-11)
 
-plt.xlabel('Frequency (MHz)')
-plt.ylabel('PSD (dB)')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('1/mW')
+plt.title("Phase noise coefficients")
 plt.legend(loc='upper right')
+
+plt.figure(4)
+plt.plot(data['freq'], coef_x_square_amp, label='square', color='b')
+plt.plot(data['freq'], coef_x_lin_amp, label='linear', color='red')
+# plt.plot(data['freq'], ratio_amp)
+
+plt.xlim(0, 2e5)
+plt.ylim(0, 1e-11)
+
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('1/mW')
+plt.title("Amplitude noise coefficients")
+plt.legend(loc='upper right')
+
 
 plt.show()
